0eaa9387aacfdcd48efc22ca856da8a608f65acd
Jeroen Dekkers <jeroen@dekkers.ch>
2012-02-22 19:14:48 +0100

    Move types to ilbc.h

--- ilbc/interface/ilbc.h
+++ ilbc/interface/ilbc.h
@@ -22,7 +22,18 @@
  * Define the fixpoint numeric formats
  */
 
-#include "typedefs.h"
+#include <stdint.h>
+
+#define G_CONST const
+
+typedef int64_t             WebRtc_Word64;
+typedef uint64_t            WebRtc_UWord64;
+typedef int32_t             WebRtc_Word32;
+typedef uint32_t            WebRtc_UWord32;
+typedef int16_t             WebRtc_Word16;
+typedef uint16_t            WebRtc_UWord16;
+typedef char                WebRtc_Word8;
+typedef uint8_t             WebRtc_UWord8;
 
 /*
  * Solution to support multiple instances
