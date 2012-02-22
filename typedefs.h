f43334af14aa873cc5edfba3a82a355abd76ed4a
Jeroen Dekkers <jeroen@dekkers.ch>
2012-02-22 14:09:05 +0100

    Add typedefs.h

---
+++ typedefs.h
@@ -0,0 +1,17 @@
+#ifndef _TYPEDEFS_H
+#define _TYPEDEFS_H
+
+#include <stdint.h>
+
+#define G_CONST const
+
+typedef int64_t             WebRtc_Word64;
+typedef uint64_t            WebRtc_UWord64;
+typedef int32_t             WebRtc_Word32;
+typedef uint32_t            WebRtc_UWord32;
+typedef int16_t             WebRtc_Word16;
+typedef uint16_t            WebRtc_UWord16;
+typedef char                WebRtc_Word8;
+typedef uint8_t             WebRtc_UWord8;
+
+#endif
