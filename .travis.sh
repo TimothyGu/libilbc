7223e8784ffdda04b7eb0daa473dd8d03a385e55
Timothy Gu <timothygu99@gmail.com>
2014-11-17 20:18:59 -0500

    travis: Test static CMake builds too
    Signed-off-by: Timothy Gu <timothygu99@gmail.com>

--- .travis.sh
+++ .travis.sh
@@ -4,4 +4,6 @@ if [ $AUTOTOOLS ]; then
     autoreconf -fi && mkdir build && cd build && ../configure && make -j2 V=1
 elif [ $CMAKE ]; then
     mkdir build && cd build && cmake .. && make -j2 VERBOSE=1
+    mkdir build && cd build && cmake .. -DBUILD_SHARED_LIBRARIES=OFF && \
+        make -j2 VERBOSE=1
 fi
