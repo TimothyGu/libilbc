9345fd6e1179dd578d253b4f8a94c5081c14f737
Timothy Gu <timothygu99@gmail.com>
2014-11-17 20:21:48 -0500

    travis: Use a separate dir for static builds for CMake
    Signed-off-by: Timothy Gu <timothygu99@gmail.com>

--- .travis.sh
+++ .travis.sh
@@ -4,6 +4,7 @@ if [ $AUTOTOOLS ]; then
     autoreconf -fi && mkdir build && cd build && ../configure && make -j2 V=1
 elif [ $CMAKE ]; then
     mkdir build && cd build && cmake .. && make -j2 VERBOSE=1
-    mkdir build && cd build && cmake .. -DBUILD_SHARED_LIBRARIES=OFF && \
+    mkdir build.static && cd build.static && \
+        cmake .. -DBUILD_SHARED_LIBRARIES=OFF && \
         make -j2 VERBOSE=1
 fi
